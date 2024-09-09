0fbf5edb97ab9bf3b19e9ecba8f27d72aa6f559b
alexBoikov <alex8oikov@yandex.ru>
2024-09-09 23:23:41 +0300

    Добавлены примеры использования List

--- ContainersExamples/Program.cs
+++ ContainersExamples/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Containers
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> integerList = new() { 2, 1, 3 };
+            Console.WriteLine("first element = " + integerList.ElementAt(0));
+            Console.WriteLine("last element = " + integerList.ElementAt(integerList.Count - 1));
+
+            integerList.Add(11);
+            integerList.Add(5);
+
+            Console.WriteLine("second element = " + integerList[1]);
+            Console.WriteLine("last element = " + integerList[integerList.Count - 1]);
+
+            Console.WriteLine($"size List = {integerList.Count}");
+
+            List<string> strings = new List<string>();
+            foreach (int value in integerList)
+            {
+                strings.Add(Convert.ToString(value));
+            }
+            Console.WriteLine($"IntegerList elements: [{String.Join(", ", strings.ToArray())}]");
+
+            integerList.Remove(11);
+            integerList.RemoveAt(3);
+
+            strings.Clear();
+            foreach (int value in integerList)
+            {
+                strings.Add(Convert.ToString(value));
+            }
+            Console.WriteLine($"After remove IntegerList elements: [{String.Join(", ", strings.ToArray())}]");
+
+            integerList.Sort();
+            strings.Clear();
+            foreach (int value in integerList)
+            {
+                strings.Add(Convert.ToString(value));
+            }
+            Console.WriteLine($"After sorting IntegerList elements: [{String.Join(", ", strings.ToArray())}]");
+        }
+    }
+}
