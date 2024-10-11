0205602b09874a1d73c59c4a1cab86e4b3796810
alexBoikov <alex8oikov@yandex.ru>
2024-10-11 15:42:44 +0300

    переименован проект оконного приложения в DemoWin добавлен класс библиотеки классов DemoLib

---
+++ Demo/Demo/DemoLib/Client.cs
@@ -0,0 +1,6 @@
+﻿namespace DemoLib
+{
+    public class Client
+    {
+    }
+}
--- Demo/Demo/DemoLib/Properties/AssemblyInfo.cs
+++ Demo/Demo/DemoLib/Properties/AssemblyInfo.cs
@@ -4,23 +4,23 @@ using System.Runtime.InteropServices;
 
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанных со сборкой.
-[assembly: AssemblyTitle("Demo")]
+// связанные со сборкой.
+[assembly: AssemblyTitle("DemoLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("Demo")]
+[assembly: AssemblyProduct("DemoLib")]
 [assembly: AssemblyCopyright("Copyright © HP 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("ea6cd0e6-8445-49db-8159-e57295edebb6")]
+[assembly: Guid("b419ae2a-88c9-4a4c-b61a-b64020f245b7")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
