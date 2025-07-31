68ec2729176d4ac3959b46d282b949df534abb9c
alexBoikov <alex8oikov@yandex.ru>
2025-07-31 20:41:06 +0300

    first MAUI empty project

---
+++ FirstMaUIApp/FirstMaUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstMaUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
