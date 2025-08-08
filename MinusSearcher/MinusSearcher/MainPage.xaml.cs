8fe8f68dc442dcad0a3d6cb1bccd677479e11943
alexBoikov <alex8oikov@yandex.ru>
2025-08-08 15:57:06 +0300

    animation pulse and wave

--- MinusSearcher/MinusSearcher/MainPage.xaml.cs
+++ MinusSearcher/MinusSearcher/MainPage.xaml.cs
@@ -1,63 +1,38 @@
-﻿namespace MinusSearcher
+﻿using SearcherCore;
+
+namespace MinusSearcher
 {
     public partial class MainPage : ContentPage
     {
-        private bool isPulsing_ = false;
+        private PulseAnimator animator_;
+        private CancellationTokenSource? cts_;
         public MainPage()
         {
             InitializeComponent();
+            animator_ = new PulseAnimator(PulseButton,
+                                          new List<VisualElement> { Wave1, Wave2});
         }
 
-        private async void OnPressed(object sender, EventArgs e)
+        private void OnPressed(object sender, EventArgs e)
         {
-            isPulsing_ = true;
-            _ = StartPulseAnimation();
-            await StartWaveLoop();
-        }
-
-        private async void OnReleased(object sender, EventArgs e)
-        {
-            isPulsing_ = false;
-
-            await PulseButton.ScaleTo(1, 200, Easing.CubicOut);
-        }
-
-        private async Task StartPulseAnimation()
-        {
-            while(isPulsing_)
+            if(cts_ != null)
             {
-                await PulseButton.ScaleTo(1.15, 300, Easing.SinInOut);
-                await PulseButton.ScaleTo(1.0, 300, Easing.SinInOut);
+                cts_.Cancel();
+                cts_.Dispose();
             }
+
+            cts_ = new CancellationTokenSource();
+            _= animator_.StartAsync(cts_.Token);
         }
 
-        private async Task StartWaveLoop()
+        private void OnReleased(object sender, EventArgs e)
         {
-            int waveIndex = 0;
-
-            while(isPulsing_)
+            if (cts_ != null)
             {
-                if (waveIndex % 2 == 0)
-                    _ = AnimateWave(Wave1, 3, 1000);
-                else
-                    _ = AnimateWave(Wave2, 4, 1200);
-
-                waveIndex++;
-                await Task.Delay(500);
+                cts_.Cancel();
+                cts_.Dispose();
+                cts_ = null;
             }
-        }
-
-        private async Task AnimateWave(Border wave, double scale, uint duration)
-        {
-            wave.Opacity = 1;
-            wave.IsVisible = true;
-
-            await Task.WhenAll(
-                wave.ScaleTo(scale, duration, Easing.SinOut),
-                wave.FadeTo(0, duration, Easing.SinOut)
-            );
-            wave.Opacity = 0;
-            wave.IsVisible = false;
         }
     }
 
---
+++ MinusSearcher/SearcherCore/PulseAnimator.cs
@@ -0,0 +1,68 @@
+﻿namespace SearcherCore
+{
+    // All the code in this file is included in all platforms.
+    public class PulseAnimator
+    {
+        private VisualElement target_; /// центральный круг
+        private List<VisualElement> waves_; /// волны
+        private CancellationToken token_;
+
+        public PulseAnimator(VisualElement targetObj,
+                             List<VisualElement> waves)
+        {
+            target_ = targetObj;
+            waves_ = waves;
+        }
+
+        public async Task StartAsync(CancellationToken token)
+        {
+            token_ = token;
+
+            _ = AnimatePulse(token_);
+
+            _ = AnimateWavesLoop(token_);
+
+            while(!token_.IsCancellationRequested)
+            {
+                await Task.Delay(100, token_);
+            }
+        }
+
+        private async Task AnimatePulse(CancellationToken token)
+        {
+            while(!token.IsCancellationRequested)
+            {
+                await target_.ScaleTo(1.15, 300, Easing.SinInOut);
+                await target_.ScaleTo(1.0, 300, Easing.SinInOut);
+            }
+        }
+
+        private async Task AnimateWavesLoop(CancellationToken token)
+        {
+            int waveIndex = 0;
+
+            while (!token.IsCancellationRequested)
+            {
+                var wave = waves_[waveIndex % waves_.Count];
+                await AnimateWave(wave, 2.0, 1200);
+
+                waveIndex++;
+                await Task.Delay(100, token);
+            }
+        }
+
+        private async Task AnimateWave(VisualElement wave, double scale, uint duration)
+        {
+            wave.Scale = 1;
+            wave.Opacity = 1;
+            wave.IsVisible = true;
+
+            await Task.WhenAll(
+                wave.ScaleTo(scale, duration, Easing.SinOut),
+                wave.FadeTo(0, duration, Easing.SinOut)
+            );
+
+            wave.IsVisible = false;
+        }
+    }
+}
